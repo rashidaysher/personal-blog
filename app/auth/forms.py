f2f9f8261f048bc65ba2c523b4c1d3aaf60da6f8
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-27 11:27:44 +0300

    imported the validationerror

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,5 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+from wtforms import ValidationError
