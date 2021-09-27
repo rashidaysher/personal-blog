de285c6d1914c3287a47e31b2c1c5b691abd53da
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-28 00:19:48 +0300

    defined the function for post

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,44 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_simplemde import SimpleMDE
 
-# Initializing application
-app = Flask(__name__,instance_relative_config= True)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
+photos = UploadSet('photos',IMAGES)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-from app import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
